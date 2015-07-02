62b36db0b4cc05615545e791eb22af2f4cdfd11b
chasmack <chas.mack@asis.com>
2015-07-02 11:35:48 -0700

    initial commit

---
+++ make.bat
@@ -0,0 +1,4 @@
+perl txt2raw.pl src/42s-cogo.txt raw/42s-cogo.raw
+perl txt2raw.pl src/42s-grid.txt raw/42s-grid.raw
+perl txt2raw.pl src/42s-grid-lc.txt raw/42s-grid-lc.raw
+perl txt2raw.pl src/42s-grid-tm.txt raw/42s-grid-tm.raw
